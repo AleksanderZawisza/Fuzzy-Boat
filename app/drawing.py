9fa4c1053c250a81c50b90875969e61a4f9da2c3
z-mrozu <zmrozu@gmail.com>
2021-12-11 20:33:27 +0100

    trail

--- app/drawing.py
+++ app/drawing.py
@@ -2,8 +2,11 @@ import pygame
 from utils import rotate_pivot
 
 
-def draw_all(screen, boat):
+def draw_all(screen, boat, alpha_surf):
     draw_background(screen)
+    draw_trail(alpha_surf, boat)
+    alpha_surf.fill((255, 255, 255, 244), special_flags=pygame.BLEND_RGBA_MULT)
+    screen.blit(alpha_surf, (0, 0))
     draw_boat(screen, boat)
     draw_info(screen, boat)
 
@@ -33,6 +36,13 @@ def draw_boat(screen, boat):
     boat_img, rect = rotate_pivot(boat_img, boat.heading, (boat.pos_x, boat.pos_y))
     # pygame.draw.rect(screen, 'yellow', rect)
     screen.blit(boat_img, rect)
+
+def draw_trail(alpha_surf, boat):
+    splash_img = pygame.image.load('assets/halftrail.png')
+    splash_img = pygame.transform.smoothscale(splash_img, (boat.length*1.2, boat.width*1.5))
+
+    splash_img, rect = rotate_pivot(splash_img, boat.heading, (boat.pos_x, boat.pos_y))
+    alpha_surf.blit(splash_img, rect)
 
 
 def draw_info(screen, boat):
